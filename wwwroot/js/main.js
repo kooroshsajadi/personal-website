1f32ffaf85c84fbd1a18fe833e053358ba349e12
Kourosh Sajjadi <kourosh_sajjadi@outlook.com>
2023-06-09 22:53:04 +0200

    Added new testimonials

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -250,24 +250,6 @@ const onscroll = (el, listener) => {
   });
 
   /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
-
-  /**
    * Animation on scroll
    */
   window.addEventListener('load', () => {
